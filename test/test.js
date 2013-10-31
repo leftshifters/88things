0826e6876a8bf46acedbf35527e28bdec8709f6f
Debjeet Biswas <debjeet@vxtindia.com>
2013-10-31 22:06:12 +0530

    Working on invoice class

---
+++ test/test.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var Invoice = require('../lib/invoice');
+
+describe('Invoice', function() {
+  describe('instance', function() {
+    var invoice = new Invoice();
+
+    it('default properties', function() {
+      assert.strictEqual(typeof invoice.number, 'string');
+      assert.strictEqual(invoice.date, 0);
+      assert.strictEqual(invoice.dueDate, 0);
+      assert.strictEqual(invoice.sentDate, 0);
+      assert.strictEqual(invoice.paid, false);
+      assert.strictEqual(invoice.notPaying, false);
+      assert.strictEqual(invoice.sent, false);
+      assert.strictEqual(Array.isArray(invoice.details), true);
+      assert.strictEqual(invoice.subtotal, 0);
+      assert.strictEqual(invoice.discount, null);
+      assert.strictEqual(Array.isArray(invoice.taxes), true);
+      assert.strictEqual(typeof invoice.currency, 'string');
+      assert.strictEqual(invoice.grandtotal, 0);
+      assert.strictEqual(typeof invoice.paymentInstruction, 'string');
+    });
+
+  });
+});
