51157763ee4cb5273f6b8d89684327856ba01b00
Debjeet Biswas <debjeet@vxtindia.com>
2013-11-01 05:34:56 +0530

    Add store class

--- test/test.js
+++ test/test.js
@@ -3,24 +3,26 @@ var Invoice = require('../lib/invoice');
 
 describe('Invoice', function() {
   describe('instance', function() {
-    var invoice = new Invoice().toData();
+    var invoice = new Invoice();
+    var invoiceData = invoice.toData();
 
     it('default properties', function() {
-      assert.strictEqual(typeof invoice.number, 'string');
-      assert.strictEqual(invoice.date, 0);
-      assert.strictEqual(invoice.dueDate, 0);
-      assert.strictEqual(invoice.sentDate, 0);
-      assert.strictEqual(invoice.paid, false);
-      assert.strictEqual(invoice.notPaying, false);
-      assert.strictEqual(invoice.sent, false);
-      assert.strictEqual(Array.isArray(invoice.details), true);
-      assert.strictEqual(invoice.subtotal, 0);
-      assert.strictEqual(invoice.discount, null);
-      assert.strictEqual(Array.isArray(invoice.taxes), true);
-      assert.strictEqual(typeof invoice.currency, 'string');
-      assert.strictEqual(invoice.grandtotal, 0);
-      assert.strictEqual(typeof invoice.paymentInstruction, 'string');
+      assert.strictEqual(typeof invoiceData.number, 'string');
+      assert.strictEqual(invoiceData.date, 0);
+      assert.strictEqual(invoiceData.dueDate, 0);
+      assert.strictEqual(invoiceData.sentDate, 0);
+      assert.strictEqual(invoiceData.paid, false);
+      assert.strictEqual(invoiceData.notPaying, false);
+      assert.strictEqual(invoiceData.sent, false);
+      assert.strictEqual(Array.isArray(invoiceData.details), true);
+      assert.strictEqual(invoiceData.subtotal, 0);
+      assert.strictEqual(invoiceData.discount, null);
+      assert.strictEqual(Array.isArray(invoiceData.taxes), true);
+      assert.strictEqual(typeof invoiceData.currency, 'string');
+      assert.strictEqual(invoiceData.grandtotal, 0);
+      assert.strictEqual(typeof invoiceData.paymentInstruction, 'string');
     });
+
 
   });
 });
